c7a87d800096605add63224e85f618046361655c
KomalParab27 <91748885+KomalParab27@users.noreply.github.com>
2021-10-04 22:08:27 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ print(math.pi)
 print("Hello")
 x = 10
 #Do something
+i am adding code here
+jus so that i can see what happens
 print('understood some of it')
 #Msake soem changes
 making some changes 9.50pm
